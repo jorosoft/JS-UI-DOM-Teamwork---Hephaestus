5823598adaeecfdbe75997c084b9f91a3bef42ec
jorosoft <jorosoftbg@gmail.com>
2016-08-11 19:38:39 +0300

    added tests, fixed bugs...

--- js/tests.js
+++ js/tests.js
@@ -2,21 +2,30 @@ QUnit.test("GameFactory_getPlayer_ShouldCreatePlayerObject", function(assert) {
     var testFactory = spaceImpactFactory(),
         testPlayer = testFactory.getPlayer("Mincho", 10, 60);
 
-    assert.notEqual(testPlayer, null);
+    assert.notEqual(testPlayer, undefined);
 });
 
-QUnit.test("GameFactory_getPlayer_ShouldCreateEnemyObject", function(assert) {
+QUnit.test("GameFactory_getEnemy_ShouldCreateEnemyObject", function(assert) {
     var testFactory = spaceImpactFactory(),
         testEnemy = testFactory.getEnemy(100, 30, 5000, 250, 50, 4, 70, 70);
 
-    assert.notEqual(testEnemy, null);
+    assert.notEqual(testEnemy, undefined);
 });
 
-QUnit.test("GameFactory_getPlayer_ShouldCreateFieldObject", function(assert) {
+QUnit.test("GameFactory_getField_ShouldCreateFieldObject", function(assert) {
     var testFactory = spaceImpactFactory(),
         testField = testFactory.getField(100, 30);
 
-    assert.notEqual(testField, null);
+    assert.notEqual(testField, undefined);
+});
+
+QUnit.test("GameEngine_projectileConsstructor_ShouldCreateProjectileObject", function(assert) {
+    var testFactory = spaceImpactFactory(),
+        testPlayer = testFactory.getField(100, 30);
+
+    var testProjectile = new projectile(20, 20, testPlayer);
+
+    assert.notEqual(testProjectile, undefined);
 });
 
 QUnit.test("GameEngine_enemyProjectileHit_ShouldReturnTrueWithCorrectParameters", function(assert) {
@@ -37,4 +46,44 @@ QUnit.test("GameEngine_enemyProjectileHit_ShouldReturnFalseWithIncorrectParamete
     var result = enemyProjectileHit(testPlayer, testProjectile);
 
     assert.notOk(result);
+});
+
+QUnit.test("GameEngine_moveShip_ShouldChangeShipCoordsWithCorrectValuesAtLeftCommand", function(assert) {
+    var testFactory = spaceImpactFactory(),
+        testPlayer = testFactory.getPlayer("Mincho", 10, 60);
+
+    var result = moveShip(testPlayer, "left");
+
+    assert.equal(testPlayer.positionX, 5);
+    assert.equal(testPlayer.positionY, 60);
+});
+
+QUnit.test("GameEngine_moveShip_ShouldChangeShipCoordsWithCorrectValuesAtRightCommand", function(assert) {
+    var testFactory = spaceImpactFactory(),
+        testPlayer = testFactory.getPlayer("Mincho", 10, 60);
+
+    var result = moveShip(testPlayer, "right");
+
+    assert.equal(testPlayer.positionX, 15);
+    assert.equal(testPlayer.positionY, 60);
+});
+
+QUnit.test("GameEngine_moveShip_ShouldChangeShipCoordsWithCorrectValuesAtUpCommand", function(assert) {
+    var testFactory = spaceImpactFactory(),
+        testPlayer = testFactory.getPlayer("Mincho", 10, 60);
+
+    var result = moveShip(testPlayer, "up");
+
+    assert.equal(testPlayer.positionX, 10);
+    assert.equal(testPlayer.positionY, 55);
+});
+
+QUnit.test("GameEngine_moveShip_ShouldChangeShipCoordsWithCorrectValuesAtDownCommand", function(assert) {
+    var testFactory = spaceImpactFactory(),
+        testPlayer = testFactory.getPlayer("Mincho", 10, 60);
+
+    var result = moveShip(testPlayer, "down");
+
+    assert.equal(testPlayer.positionX, 10);
+    assert.equal(testPlayer.positionY, 65);
 });
